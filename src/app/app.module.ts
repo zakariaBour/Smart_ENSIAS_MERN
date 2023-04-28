ebfb31d6233af36df6115d1a5597de9210848646
zawa9 <zakaria.bourhil@um5r.ac.ma>
2023-04-28 21:38:44 +0100

    push students components in my repo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainComponent } from './main/main.component';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SidebarComponent,
-    MainComponent
+    MainComponent,
+    AddStudentComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: MainComponent },
+      { path: 'studentdetails', component: StudentListComponent },
+      { path: 'Addstudent', component: AddStudentComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
