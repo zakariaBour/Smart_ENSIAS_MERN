e3153ad37e6cb1afdb070442fff2ef215731bdb5
AbderrazzakNfissi <abderrazzak.nfissi@usmba.ac.ma>
2023-04-28 19:48:58 +0100

    add sidebar, navbar, main components

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'SmartEnsias'`, () => {
+  it(`should have as title 'smart-enais'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SmartEnsias');
+    expect(app.title).toEqual('smart-enais');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('SmartEnsias app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('smart-enais app is running!');
   });
 });
