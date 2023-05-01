9bed3a9b429a31410ba841f2018753d8d746f537
zawa9 <zakaria.bourhil@um5r.ac.ma>
2023-05-01 16:43:12 +0100

    added search functionnality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,13 @@ import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TeacherListComponent } from './teacher-list/teacher-list.component';
 import { AddTeacherComponent } from './add-teacher/add-teacher.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { ToastrModule } from 'ngx-toastr';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StudentService } from './services/student.service';
 import { EditStudentComponent } from './edit-student/edit-student.component';
+import { SearchFlterPipe } from './search-flter.pipe';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -26,9 +26,9 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'studentdetails', component: StudentListComponent },
   { path: 'Addstudent', component: AddStudentComponent },
-  { path:'teachers',component : TeacherListComponent},
-  { path: 'addTeacher',component : AddTeacherComponent},
-  { path :'editStudent/:cne',component : EditStudentComponent}
+  { path: 'teachers', component: TeacherListComponent },
+  { path: 'addTeacher', component: AddTeacherComponent },
+  { path: 'editStudent/:cne', component: EditStudentComponent }
 ];
 
 @NgModule({
@@ -43,7 +43,7 @@ const routes: Routes = [
     TeacherListComponent,
     AddTeacherComponent,
     EditStudentComponent,
-    
+    SearchFlterPipe,
   ],
   imports: [
     BrowserModule,
@@ -52,7 +52,8 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     ReactiveFormsModule,
     ToastrModule.forRoot(),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [
     StudentService
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
 })
 
 export class NavbarComponent {
-
+  constructor(public searchService: SearchService) { }
 }
