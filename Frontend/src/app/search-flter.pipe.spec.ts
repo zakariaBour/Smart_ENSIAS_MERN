6a7c4332c62a332708206c643a8ef2487c740e64
zawa9 <zakaria.bourhil@um5r.ac.ma>
2023-05-04 15:29:36 +0100

    restructure project again

--- Frontend/src/app/search-flter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SearchFlterPipe } from './search-flter.pipe';
-
-describe('SearchFlterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SearchFlterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
